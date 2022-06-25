a67e9ae2ebf7462e9b224dcc0545c0507d8a1bc4
engnraminul <engnraminul@gmail.com>
2022-06-25 22:02:15 +0600

    Create ChatMessage models

--- Chat/admin.py
+++ Chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Friend
+from .models import Profile, Friend, ChatMessage
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Friend)
+admin.site.register(ChatMessage)
--- Chat/models.py
+++ Chat/models.py
@@ -16,3 +16,13 @@ class Friend(models.Model):
 
     def __str__(self):
         return self.profile.name
+
+
+class ChatMessage(models.Model):
+    body = models.TextField()
+    msg_sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="msg_sernder")
+    msg_receciver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="msg_receiver")
+    seen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.body
