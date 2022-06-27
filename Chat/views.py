1186ad2c09688864f596d3e7bb425e4c02b9d20a
engnraminul <engnraminul@gmail.com>
2022-06-27 21:01:14 +0600

    sending chat message

--- Chat/views.py
+++ Chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from Chat.models import Friend, Profile
 from .forms import ChatMessageForm
@@ -15,11 +15,23 @@ def home(request):
     return render(request, 'home.html', context)
 
 
-def detail(request, pk):
+def detail(request,pk):
     friend = Friend.objects.get(id=pk)
+    user = request.user.profile
+    profile = Profile.objects.get(id=friend.profile.id)
     chatform = ChatMessageForm()
+    if request.method == "POST":
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            chat_message = form.save(commit=False)
+            chat_message.msg_sender = user
+            chat_message.msg_receciver = profile
+            chat_message.save()
+            return redirect("detail", pk=friend.profile.id)
     context = {
         "friend":friend,
         "chatform":chatform,
+        "user":user,
+        "profile":profile,
     }
     return render(request, "detail.html", context)
